c694158bd7ed05c8cba9e61a7dce656389e3b33b
Conner Hufnagel <conner.huf@gmail.com>
2024-05-21 13:25:38 -0700

    added client
    I added a frontend UI for interacting with the ConnerAPI backend i made last week. Super basic stuff, but it makes it easier to show to people.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const BASE_URL = 'https://app-connerapi-dev.azurewebsites.net';
+
+  const ACTIVE_URL = BASE_URL;
+
+  const [apiResponse, setApiResponse] = useState({});
+
+  function callResume() {
+    fetch(`${ACTIVE_URL}/resume`)
+      .then(response => response.json())
+      .then(data => setApiResponse(data))
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  function callWorkXp() {
+    fetch(`${ACTIVE_URL}/workexperience`)
+      .then(response => response.json())
+      .then(data => setApiResponse(data))
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  function callProjects() {
+    fetch(`${ACTIVE_URL}/projects`)
+      .then(response => response.json())
+      .then(data => setApiResponse(data))
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  function callStory() {
+    fetch(`${ACTIVE_URL}/story`)
+      .then(response => response.json())
+      .then(data => setApiResponse(data))
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1 className='welcome'>Welcome to the ConnerAPI</h1>
+        <p className='welcome-description'>Here, you can find all things "Conner" (the full-stack dev you've dreamed of). Click the buttons below to call the API, and finally be introduced to the candidate you've been waiting for.</p>
+      </div>
+      
+      <ul className="api-options">
+      <li className='api-option-item'>
+          <h4>Get My Story</h4>
+          <button onClick={callStory}>Story</button>
+        </li>
+        <li className='api-option-item'>
+          <h4>Get Full Resume</h4>
+          <button onClick={callResume}>Resume</button>
+        </li>
+        <li className='api-option-item'>
+          <h4>Get Work Experience</h4>
+          <button onClick={callWorkXp}>Work</button>
+        </li>
+        <li className='api-option-item'>
+          <h4>Get Projects</h4>
+          <button onClick={callProjects}>Projects</button>
+        </li>
+      </ul>
+
+      <div className='api-result'>
+        {apiResponse && <pre className='json-response'>{JSON.stringify(apiResponse, null, 2)}</pre>}
+      </div>
     </div>
   );
 }
